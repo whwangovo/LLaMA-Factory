e9b8ccce103cbf2c898ae77584c650762d609c92
kiren <wwkml994@gmail.com>
2025-03-06 14:46:38 +0000

    merge

--- scripts/cal_ppl.sh
+++ scripts/cal_ppl.sh
@@ -3,12 +3,17 @@
 torchrun --nproc_per_node=1 \
     scripts/cal_ppl.py \
 <<<<<<< HEAD
+<<<<<<< HEAD
     --model_name_or_path bidding_outputs/finetune_outputs/qwen_pretrain_finetune_241230 \
     --dataset bidding_test_ppl \
 =======
     --model_name_or_path /home/lt_08321/hdd/wangweihang/outputs/saves/241216/qwen2.5-14b/full/pt \
     --dataset pretrain_241216 \
 >>>>>>> ad1c2617 (bidding)
+=======
+    --model_name_or_path /home/lt_08321/hdd/wangweihang/outputs/saves/241216/qwen2.5-14b/full/pt \
+    --dataset pretrain_241216 \
+>>>>>>> 6b4f8743 (merge)
     --batch_size 1 \
     --template qwen \
     --cutoff_len 4096 \
