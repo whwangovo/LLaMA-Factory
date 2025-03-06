581037828e27847f952035cd6e6dca1e594312df
kiren <wwkml994@gmail.com>
2025-03-06 14:44:39 +0000

    merge

---
+++ vllm_deploy/api.sh
@@ -0,0 +1,3 @@
+# CUDA_VISIBLE_DEVICES=2 vllm serve /home/lt_08321/hdd/wangweihang/checkpoints/Qwen/Qwen2.5-14B-Instruct
+# CUDA_VISIBLE_DEVICES=2 vllm serve /home/lt_08321/hdd/wangweihang/outputs/saves/qwen2.5-14b/full/sft
+CUDA_VISIBLE_DEVICES=2 vllm serve /home/lt_08321/hdd/wangweihang/outputs/saves/qwen2.5-14b/full/sft_241203
