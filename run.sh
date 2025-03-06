7fd0b534afe6221f6b3b96a2cd6f1669d88d7eee
kiren <wwkml994@gmail.com>
2025-03-06 14:50:00 +0000

    merge

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# CUDA_VISIBLE_DEVICES=0,1 llamafactory-cli train examples/train_full/qwen25_full_sft.yaml
-CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 llamafactory-cli train /home/lt_08321/ssd/wangweihang/LLaMA-Factory/examples/train_full/qwen25_full_pretrain.yaml
