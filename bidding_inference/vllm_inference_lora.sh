74efe3b5fcfd1b5b71e2d872e345794a072e8817
kiren <wwkml994@gmail.com>
2025-03-10 11:05:49 +0000

    32b

--- bidding_inference/vllm_inference_lora.sh
+++ bidding_inference/vllm_inference_lora.sh
@@ -1,6 +1,6 @@
 # API_PORT=8000 llamafactory-cli api bidding_srcipts/bidding_inference.yaml
-vllm serve checkpoints/Qwen/Qwen2.5-14B-Instruct --tensor-parallel-size 2 \
+vllm serve checkpoints/Qwen/Qwen2.5-32B-Instruct --tensor-parallel-size 2 \
                                                  --max-model-len 16384 \
                                                  --enable-lora \
-                                                 --lora-modules rw=bidding_outputs/finetune_outputs/finetune@lora@realworld@250228 \
-                                                 cs=bidding_outputs/finetune_outputs/finetune@lora@continue_summary@250228
+                                                 --lora-modules rw=bidding_outputs/finetune_outputs/finetune@32B@lora@pure@FGIRCS@20250310 \
+                                                 # cs=bidding_outputs/finetune_outputs/finetune@lora@continue_summary@250228
