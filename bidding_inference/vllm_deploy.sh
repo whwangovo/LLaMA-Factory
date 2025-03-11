9b7dcaeb1a8c8ab799f93866386e2e12924a71e2
kiren <wwkml994@gmail.com>
2025-03-11 17:24:23 +0000

    prt

--- bidding_inference/vllm_deploy.sh
+++ bidding_inference/vllm_deploy.sh
@@ -1,5 +1,5 @@
 # API_PORT=8000 llamafactory-cli api bidding_srcipts/bidding_inference.yaml
-vllm serve bidding_outputs/finetune_outputs/finetune@pt@FGIRCS@20250306 \
+vllm serve checkpoints/Qwen/Qwen2.5-32B-Instruct \
                                                  --tensor-parallel-size 8 \
                                                  --served-model-name qwen_chat \
                                                  --port 8000
