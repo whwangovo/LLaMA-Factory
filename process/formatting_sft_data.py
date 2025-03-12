3f9df5cfa68de22f04f1c6883764c3a7a3b039ae
kiren <wwkml994@gmail.com>
2025-03-12 13:05:51 +0000

    cpt_2

--- process/formatting_sft_data.py
+++ process/formatting_sft_data.py
@@ -11,8 +11,9 @@ def transform_json(input_file, output_file):
         transformed_entry = {
             "instruction": entry.get("question", ""),
             "input": "",
-            "chosen": entry.get("answer", ""),
-            "rejected": entry.get("predict", ""),
+            "output": entry.get("answer", "")
+            # "chosen": entry.get("answer", ""),
+            # "rejected": entry.get("predict", ""),
         }
         transformed_data.append(transformed_entry)
 
@@ -24,6 +25,6 @@ def transform_json(input_file, output_file):
 
 # Example usage
 if __name__ == "__main__":
-    input_json_file = "data/dpo@zhulong@2@20250306_predict.json"  # Replace with your input JSON file
-    output_json_file = "data/dpo@zhulong@2@20250306_predict.json"  # Replace with your desired output JSON file
+    input_json_file = "data/archived/zhulong@2@20250225_original.json"  # Replace with your input JSON file
+    output_json_file = "data/finetune@zhulong@2@20250306.json"  # Replace with your desired output JSON file
     transform_json(input_json_file, output_json_file)
